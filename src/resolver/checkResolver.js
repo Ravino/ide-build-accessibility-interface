eab3ace1975207c8e3fb1306fe46fc892b678759
Ravino Doul <evgeniy@nekrasov.pw>
2021-06-09 14:11:52 +1000

    Added base resolver for check route

---
+++ src/resolver/checkResolver.js
@@ -0,0 +1,12 @@
+const escapeHtml = require('escape-html');
+const trim = require('trim');
+
+class CheckResolver {
+
+  done() {
+    return undefined;
+  }
+}
+
+
+module.exports = new CheckResolver();
