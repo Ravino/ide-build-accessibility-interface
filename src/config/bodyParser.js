7e4f14b508a8089404c918082762ca7dcc2e35fc
Ravino Doul <evgeniy@nekrasov.pw>
2021-06-09 14:09:34 +1000

    Added config body parser

---
+++ src/config/bodyParser.js
@@ -0,0 +1,8 @@
+const bodyParser = require("body-parser");
+
+
+module.exports = function(server) {
+  server.use(bodyParser.urlencoded({extended: true}));
+  server.use(bodyParser.json());
+  return undefined;
+}
