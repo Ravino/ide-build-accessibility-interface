12bb0e6aa239c64d9f153a30de2ad2257a16f628
Ravino Doul <evgeniy@nekrasov.pw>
2021-06-16 10:21:36 +1000

    Connected validator and parse dom

--- src/resolver/checkResolver.js
+++ src/resolver/checkResolver.js
@@ -1,9 +1,41 @@
 const escapeHtml = require('escape-html');
 const trim = require('trim');
 
+const domService = require('../service/domService');
+
+const StatusView = require('../view/statusView');
+
+
 class CheckResolver {
 
-  done() {
+  constructor() {
+    this.domService = domService;
+    this.statusView = new StatusView();
+  }
+
+
+  async parse(str) {
+
+    str = trim(str);
+
+
+    const validate = await this.domService.validate(str);
+    if(validate) {
+      this.statusView.addStatus('invalidSyntax');
+      this.statusView.addData(validate);
+      return this.statusView;
+    }
+
+
+    const domTree = this.domService.parse(str);
+    console.log(domTree);
+    return domTree;
+  }
+
+
+  async done(req, res) {
+    const result = await this.parse(req.body.html);
+    res.json(result);
     return undefined;
   }
 }
